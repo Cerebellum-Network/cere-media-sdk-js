fa48b4f501145567c9d9c0404930c65ad94b4208
mfbevan <matthewbevan00@gmail.com>
2024-01-17 13:26:29 +1000

    feat: ✨ setup playground app

---
+++ playground/src/App.tsx
@@ -0,0 +1,15 @@
+import cereLogo from '/cere.png';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={cereLogo} className="logo" alt="Cere logo" />
+        <p>Cere Media SDK </p>
+      </div>
+    </>
+  );
+}
+
+export default App;

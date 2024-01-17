e9e59b1761a5626bfac69cd2870a0822c56caf91
mfbevan <matthewbevan00@gmail.com>
2024-01-17 17:48:11 +1000

    fix: 🐛 browser compatible logger

--- packages/client/src/services/logger.service.ts
+++ packages/client/src/services/logger.service.ts
@@ -1,7 +1,12 @@
-import PinoLogger from 'pino';
-import Pretty from 'pino-pretty';
-
 export const Logger = (name: string, enabled: boolean = false) => {
+  if (typeof window !== 'undefined') {
+    // Browser logger
+    return console;
+  }
+
+  const PinoLogger = require('pino');
+  const Pretty = require('pino-pretty');
+
   return PinoLogger(
     Pretty({
       colorize: true,
@@ -11,7 +16,7 @@ export const Logger = (name: string, enabled: boolean = false) => {
   );
 };
 
-export type Logger = PinoLogger.Logger;
+export type Logger = typeof console;
 
 export const handleError = (logger: Logger, message?: string) => (error: Error) => {
   logger.error(error.message);

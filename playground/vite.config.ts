648dddb6881813259966db8ee8f90dd3bd0be871
mfbevan <matthewbevan00@gmail.com>
2024-01-19 10:18:04 +1000

    fix: 🐛 vite config for browser polyfills

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, ResolveOptions, PluginOption } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+/**
+ * https://vitejs.dev/config/
+ */
+export default defineConfig(({ command }) => {
+  const resolve: ResolveOptions = {};
+  const plugins: PluginOption[] = [
+    react(),
+    nodePolyfills({
+      globals: {
+        Buffer: false,
+      },
+    }),
+  ];
+
+  /**
+   * Build SDK from source code in development mode
+   */
+  if (command === 'serve') {
+    resolve.extensions = ['.web.js', '.js', '.web.ts', '.ts', '.jsx', '.tsx', '.json'];
+    plugins.push(tsconfigPaths());
+  }
+
+  return {
+    base: './',
+    plugins,
+    resolve,
+
+    define: {
+      'process.env.BC_ENDPOINT': JSON.stringify('ws://localhost:9944'),
+    },
+  };
+});

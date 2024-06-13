7af0f43b34a1bf880779535eb15e63f015484a1a
antonmazhuto <anton.mazhuto@cere.io>
2024-06-13 17:40:47 +0300

    Fixed tests

--- playground/src/components/audio-example/AudioExample.tsx
+++ playground/src/components/audio-example/AudioExample.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { EncryptedAudioPlayer } from '@cere/media-sdk-react';
 import { CircularProgress } from '@mui/material';
+import { ChainNamespace } from '@cere/media-sdk-client';
 
 export const AudioExample = () => {
   return <EncryptedDdcAudio />;
@@ -19,5 +20,7 @@ const EncryptedDdcAudio = () => (
     assetIndex={assetIndex}
     loadingComponent={<CircularProgress />}
     playerOverrides={{ autoPlay: true }}
+    chainId="8002"
+    chainNamespace={ChainNamespace.EIP155}
   />
 );

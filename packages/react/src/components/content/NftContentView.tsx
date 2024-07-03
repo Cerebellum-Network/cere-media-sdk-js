a05dc5b49da0e234bd66e0b546c50a54434798f0
antonmazhuto <anton.mazhuto@cere.io>
2024-07-03 17:11:21 +0300

    Prepare legacy release

--- packages/react/src/components/content/NftContentView.tsx
+++ packages/react/src/components/content/NftContentView.tsx
@@ -1,8 +1,9 @@
 import { NFT, NftMetadata } from '@cere/media-sdk-client';
-import { useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
 import { EncryptedAudioPlayer, HlsEncryptionLoader, VideoPlayer } from '..';
-import { useEncryptedContent, useMediaClient } from '../../hooks';
+import { useDownloadContent, useEncryptedContent, useMediaClient } from '../../hooks';
+import './styles.css';
 
 export interface ContentViewProps {
   /**
@@ -20,12 +21,71 @@ export interface ContentViewProps {
 
   loadingComponent?: React.ReactNode;
 
-  classNames?: Partial<Record<'container' | 'imageBlock' | 'image', string>>;
+  classNames?: Partial<Record<'container' | 'content' | 'imageBlock' | 'image', string>>;
+
+  callback?: () => void;
+
+  DownloadIcon?: React.FC<React.SVGProps<SVGSVGElement>>;
 }
 
-export const NftContentView = ({ nft, metadata, assetIndex, loadingComponent, classNames }: ContentViewProps) => {
+const useCustomDownloadContent = ({
+  content,
+  collectionAddress,
+  nftId,
+}: {
+  content?: string;
+  nftId: number;
+  collectionAddress: string;
+}) => {
+  if (!content) return;
+
+  const { download } = useDownloadContent(
+    { nftId: Number(nftId), collection: { address: collectionAddress } } as NFT,
+    `asset-${content.split('/').pop()}`,
+  );
+
+  return { download };
+};
+
+export const NftContentView = ({
+  nft,
+  metadata,
+  assetIndex,
+  loadingComponent,
+  classNames,
+  callback,
+  DownloadIcon,
+}: ContentViewProps) => {
+  const [isLoadingAsset, setLoadingAsset] = useState(false);
+
   const { client, isLoading: isLoadingClient } = useMediaClient();
   const { content, isLoading, isVideo, contentType, asset } = useEncryptedContent(nft, metadata, assetIndex);
+
+  const { download } =
+    useCustomDownloadContent({
+      nftId: Number(nft.nftId),
+      collectionAddress: nft.collection.address!,
+      content,
+    }) || {};
+
+  const handleImageLoad = useCallback(() => {
+    if (callback) {
+      callback();
+    }
+  }, [callback]);
+
+  const handleOnDownload = useCallback(async () => {
+    setLoadingAsset(true);
+    try {
+      if (download) {
+        await download();
+        setLoadingAsset(false);
+      }
+    } catch (e) {
+      console.log('An error occurred while loading an asset. Try again later');
+      setLoadingAsset(false);
+    }
+  }, [download]);
 
   const isStreamableAudio = !!contentType && contentType.includes('audio');
 
@@ -53,46 +113,62 @@ export const NftContentView = ({ nft, metadata, assetIndex, loadingComponent, cl
     );
   }
 
-  if (!isVideo && !isStreamableAudio) {
-    return (
-      <div className={classNames?.imageBlock || ''}>
-        {!content ? (
-          loadingComponent ? (
-            loadingComponent
+  const renderContent = useMemo(() => {
+    if (!isVideo && !isStreamableAudio) {
+      return (
+        <div className={classNames?.imageBlock || ''}>
+          {!content ? (
+            loadingComponent ? (
+              loadingComponent
+            ) : (
+              <div className="loading-container">Loading...</div>
+            )
           ) : (
-            <div className="loading-container">Loading...</div>
-          )
-        ) : (
-          <img className={classNames?.image || ''} alt={asset?.name} src={content} width="100%" height="100%" />
-        )}
-      </div>
-    );
-  }
+            <img
+              className={classNames?.image || ''}
+              alt={asset?.name}
+              src={content}
+              width="100%"
+              height="100%"
+              onLoad={handleImageLoad}
+            />
+          )}
+        </div>
+      );
+    }
+    if (isStreamableAudio) {
+      return (
+        <EncryptedAudioPlayer
+          className={classNames?.container}
+          nftId={nft.nftId}
+          collectionAddress={nft.collection.address}
+          assetIndex={assetIndex}
+          loadingComponent={loadingComponent}
+        />
+      );
+    }
 
-  if (isStreamableAudio) {
-    return (
-      <EncryptedAudioPlayer
-        className={classNames?.container}
-        nftId={nft.nftId}
-        collectionAddress={nft.collection.address}
-        assetIndex={assetIndex}
-        loadingComponent={loadingComponent}
-      />
-    );
-  }
+    if (isVideo) {
+      return (
+        <VideoPlayer
+          className={classNames?.container}
+          src={asset.asset}
+          loader={loader}
+          hlsEnabled={true}
+          loadingComponent={loadingComponent}
+        />
+      );
+    }
+  }, []);
 
-  if (isVideo) {
-    return (
-      <VideoPlayer
-        className={classNames?.container}
-        src={asset.asset}
-        loader={loader}
-        hlsEnabled={true}
-        loadingComponent={loadingComponent}
-      />
-    );
-  }
-
-  console.error(`Unhandled media type ${contentType}`);
-  return <>Unhandled media type {contentType}</>;
+  return (
+    <div className="flex self-stretch">
+      <div className="w-full content">{renderContent}</div>
+      {DownloadIcon && (
+        <button className="download-content" onClick={handleOnDownload} disabled={isLoadingAsset}>
+          {isLoadingAsset ? loadingComponent : <DownloadIcon />}
+        </button>
+      )}
+    </div>
+  );
 };

4faba243cdb2558ae74ca92fc5a01f440a4c522e
antonmazhuto <anton.mazhuto@cere.io>
2024-08-22 17:08:08 +0300

    Metamask wallet support

--- playground/src/components/playground/PlaygroundNavigation.tsx
+++ playground/src/components/playground/PlaygroundNavigation.tsx
@@ -4,7 +4,6 @@ import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import { useState } from 'react';
-import { CheckNft } from './CheckNft.tsx';
 
 export const PlaygroundNavigation = () => {
   const [value, setValue] = useState('0');

47f92ef6803e43a6fcff6ee3d611347cc2a9801e
antonmazhuto <anton.mazhuto@cere.io>
2024-08-28 18:16:38 +0300

    Improve bundle size

--- packages/react/src/components/video/HlsEncryptionLoader.tsx
+++ packages/react/src/components/video/HlsEncryptionLoader.tsx
@@ -1,27 +1,35 @@
 import { MediaSdkClient } from '@cere/media-sdk-client';
 import { blake2b } from 'blakejs';
-import Hls, { LoaderCallbacks, LoaderConfiguration, LoaderContext, LoaderResponse, LoaderStats } from 'hls.js';
+import type {
+  HlsConfig,
+  LoaderCallbacks,
+  LoaderConfiguration,
+  LoaderContext,
+  LoaderResponse,
+  LoaderStats,
+} from 'hls.js';
 import nacl from 'tweetnacl';
 
-interface EncryptionConfig {
-  collectionAddress: string;
-  nftId: number;
-  assetId: string;
-  client: MediaSdkClient;
-}
+let Hls: typeof import('hls.js').default | null = null;
 
-export class HlsEncryptionLoader extends Hls.DefaultConfig.loader {
+export class HlsEncryptionLoader {
   private static client: MediaSdkClient;
-
   public static collectionAddress: string;
-
   public static nftId: number;
-
   public static assetId: string;
-
   public static dek?: Uint8Array;
 
-  public static create({ collectionAddress, nftId, assetId, client }: EncryptionConfig) {
+  public static create({
+    collectionAddress,
+    nftId,
+    assetId,
+    client,
+  }: {
+    collectionAddress: string;
+    nftId: number;
+    assetId: string;
+    client: MediaSdkClient;
+  }) {
     this.collectionAddress = collectionAddress;
     this.nftId = nftId;
     this.assetId = assetId;
@@ -31,11 +39,29 @@ export class HlsEncryptionLoader extends Hls.DefaultConfig.loader {
     return this;
   }
 
-  load(context: LoaderContext, config: LoaderConfiguration, callbacks: LoaderCallbacks<LoaderContext>) {
+  async load(context: LoaderContext, config: LoaderConfiguration, callbacks: LoaderCallbacks<LoaderContext>) {
+    if (!Hls) {
+      const hlsModule = await import('hls.js');
+      Hls = hlsModule.default;
+    }
+
+    const baseLoaderClass = Hls.DefaultConfig.loader;
+
+    if (!baseLoaderClass) {
+      throw new Error('HLS base loader class is not available.');
+    }
+
+    const fullConfig: LoaderConfiguration & HlsConfig = {
+      ...Hls.DefaultConfig,
+      ...config,
+    };
+
+    const baseLoaderInstance = new baseLoaderClass(fullConfig);
+
     const { baseUrl, nonce, bucketId, cid, encrypted, isChunk } = this.destructureChunkCid(context.url);
 
     if (!isChunk) {
-      super.load(context, config, callbacks);
+      baseLoaderInstance.load(context, fullConfig, callbacks);
       return;
     }
 
@@ -43,7 +69,7 @@ export class HlsEncryptionLoader extends Hls.DefaultConfig.loader {
       context.url = `${baseUrl}/${bucketId}/${cid}`;
     }
 
-    super.load(context, config, {
+    baseLoaderInstance.load(context, fullConfig, {
       ...callbacks,
       onSuccess: async (
         response: LoaderResponse,
@@ -67,13 +93,6 @@ export class HlsEncryptionLoader extends Hls.DefaultConfig.loader {
       },
     });
   }
-
-  /**
-   * Decrypt a chunk of encrypted data using the nonce encoded in the playlist file and the master DEK associated with this video
-   * @param data The encrypted data to be decrypted
-   * @param nonce The nonce for the chunk to be decrypted. This is encoded in the chunk ID of the playlist file
-   * @returns The decrypted chunk
-   */
   private async decrypt(data: Uint8Array, nonce: Uint8Array): Promise<ArrayBuffer> {
     const masterDek = await HlsEncryptionLoader.fetchDek();
     const derivedDek = this.deriveDek(nonce, masterDek);
@@ -88,12 +107,6 @@ export class HlsEncryptionLoader extends Hls.DefaultConfig.loader {
     return Buffer.from(result.buffer);
   }
 
-  /**
-   * Derive a dek for the chunk from the master dek and the chunk nonce
-   * @param nonce The nonce for the chunk to be decrypted. This is encoded in the chunk ID of the playlist file
-   * @param masterDek The master DEK that was used to encrypt the file
-   * @returns The derived DEK
-   */
   public deriveDek(nonce: Uint8Array, masterDek: Uint8Array): Uint8Array {
     const seed = new Uint8Array(nonce.length + masterDek.length);
     seed.set(nonce);
@@ -102,9 +115,6 @@ export class HlsEncryptionLoader extends Hls.DefaultConfig.loader {
     return this.blake2AsU8a(seed);
   }
 
-  /**
-   * Fetches the root dek that was used to encrypt this video, each chunk derives its own dek from this root dek
-   */
   private static async fetchDek(): Promise<Uint8Array> {
     if (this.dek) {
       return this.dek;
@@ -120,12 +130,6 @@ export class HlsEncryptionLoader extends Hls.DefaultConfig.loader {
     return this.dek;
   }
 
-  /**
-   * When chunks are encrypted they have their encryption nonce prepended to the CID,
-   * e.g chunk/enc_{{nonce}}_{{cid}}}
-   * e.g chunk/enc_0x01decbcb3f8908d885b5a5663c8786f73c240dc4ed94df55_baebb4iddto7txzjtxwsmriyghknrrzhcnjroevdlngcmdllb7orluhl4wy
-   * @param chunkCid Full encoded CID of the chunk pulled from the playlist file
-   */
   private destructureChunkCid(apiUrl: string) {
     const url = new URL(apiUrl);
     const isChunk = apiUrl.includes('chunk');
@@ -154,27 +158,18 @@ export class HlsEncryptionLoader extends Hls.DefaultConfig.loader {
     };
   }
 
-  //
-  // The following helpers were recreated from the @polkadot/... packages as the some webpack configs do not support using these packages directly
-  // They directly use the tweetnacl and blakejs libraries
-  //
-
   private static hexToU8a(hexString: string): Uint8Array {
-    // Ensure the input is a string
     if (typeof hexString !== 'string') {
       throw new Error('Input must be a string');
     }
 
-    // Remove the '0x' prefix if present
     hexString = hexString.startsWith('0x') ? hexString.slice(2) : hexString;
 
-    // Ensure the string length is even
     if (hexString.length % 2 !== 0) {
       console.warn('Hex string has an odd number of characters, adding a leading zero.');
       hexString = '0' + hexString;
     }
 
-    // Convert the hexadecimal string to a Uint8Array
     const length = hexString.length / 2;
     const result = new Uint8Array(length);
 
